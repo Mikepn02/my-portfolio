b2d882c041a732b0838aeef76f0599ebf6e4fe2b
mikepn02 <nzaberamikepeter@gmail.com>
2025-10-23 22:45:43 +0200

    (fix): build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Mike | Person Portfolio",
+  title: "Mike Nzabera | Person Portfolio",
 };
 
 export default function RootLayout({
